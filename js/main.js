d34ed7313f3bda0878343f8e77927d77fcd70245
Sebastian Garcia <sebastiangarcia1105@gmail.com>
2025-04-10 14:27:12 -0300

    Se le mejoro la pregunta del if ternario y ahora acepta 4 caracteres en vez de solamente 3

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const productos = []
 const ingresarProducto = () => {
     for (let i = 0; i < 3; i++) {
         let favProd = prompt("Ingrese su producto favorito")
-        favProd.length < 3
+        favProd.length <= 3
         ? (alert("El nombre del producto es muy corto"), i--)
         : productos.push(favProd)
     }
