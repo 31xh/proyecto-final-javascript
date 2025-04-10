18285e75d11e8cc12d3b36b5975023b3b360ff1a
Sebastian Garcia <sebastiangarcia1105@gmail.com>
2025-04-10 14:09:41 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const productos = []
+
+const ingresarProducto = () => {
+    for (let i = 0; i < 3; i++) {
+        let favProd = prompt("Ingrese su producto favorito")
+        favProd.length < 3
+        ? (alert("El nombre del producto es muy corto"), i--)
+        : productos.push(favProd)
+    }
+}
+
+const mostrarProductos = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`${i + 1} ${arr[i].toUpperCase()}`)
+    }
+}
+
+ingresarProducto()
+mostrarProductos(productos)
