d56e4039ab52e03458a834e813414b120564b550
Sebastian Garcia <sebastiangarcia1105@gmail.com>
2025-04-10 14:52:13 -0300

    Se mejoro la logica del javascript y se le agregaron validaciones

--- js/main.js
+++ js/main.js
@@ -2,11 +2,26 @@ const productos = []
 
 const ingresarProducto = () => {
     for (let i = 0; i < 3; i++) {
-        let favProd = prompt("Ingrese su producto favorito")
-        favProd.length <= 3
-        ? (alert("El nombre del producto es muy corto"), i--)
-        : productos.push(favProd)
+        let favProd = prompt("Ingrese su producto favorito").trim()
+        let isValid = validaciones(favProd)
+        isValid ?
+            (productos.push(favProd), alert(`El producto: ${favProd} ha sido agregado con exito`))
+            : ("Producto no validado", i--)
     }
+}
+
+const validaciones = (arg) => {
+    if (!arg) {
+        alert("El producto no puede estar vacío");
+        return false;
+    } else if (!isNaN(arg)) {
+        alert("El producto no debe ser un número");
+        return false;
+    } else if (arg.length < 4) {
+        alert("El nombre del producto debe tener al menos 4 caracteres");
+        return false;
+    }
+    return true;
 }
 
 const mostrarProductos = (arr) => {
